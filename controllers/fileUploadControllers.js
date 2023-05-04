e53d878342b0418beb49c1bacac4bea09ae7c8e1
katoragashua <katoragashua@outlook.com>
2023-05-04 09:00:34 +0100

    AI auto tagging for photos

--- controllers/fileUploadControllers.js
+++ controllers/fileUploadControllers.js
@@ -6,8 +6,16 @@ const exifr = require("exifr");
 const { log } = require("console");
 const CustomError = require("../errors/index");
 const exif = require("fast-exif");
+const ImageKit = require("imagekit");
 
-// cloudinary configuration
+// Imagekit SDK configuration
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGE_KIT_URL,
+});
+
+// cloudinary SDK configuration
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
@@ -62,10 +70,15 @@ const uploadImage = async (req, res) => {
   const maxSize = 1024 * 1024 * 10;
   if (img.size > maxSize)
     throw new CustomError.BadRequestError("Image must be less than 5mb.");
-
+  // Uploading to cloudinary
   const result = await cloudinary.uploader.upload(
     req.files.image.tempFilePath,
-    { use_filename: true, folder: "file_upload" }
+    {
+      use_filename: true,
+      folder: "file_upload",
+      categorization: "aws_rek_tagging",
+      auto_tagging: 0.7,
+    }
   );
   // const exifData = await exifr.parse(req.files.image.tempFilePath);
   const exifData = await exif.read(req.files.image.tempFilePath);
@@ -76,14 +89,53 @@ const uploadImage = async (req, res) => {
       "Photo has no metadata. Please upload only photos taken from a camera."
     );
   }
-
+  const { image, exif: more } = exifData;
+  console.log(image, more);
+  // get orientation
+  // const orientation = await exifr.orientation(req.files.image.tempFilePath);
   // Removing the tempfiles
   fs.unlinkSync(req.files.image.tempFilePath);
 
-  res
-    .status(StatusCodes.OK)
-    .json({ image: { src: result.secure_url }, exifData });
+  res.status(StatusCodes.OK).json({ image: { src: result }, exifData });
 };
+
+// const uploadImage = async (req, res) => {
+//   if (!req.files) throw new CustomError.BadRequestError("No file uploaded.");
+
+//   const img = req.files.image;
+//   if (!img.mimetype.startsWith("image"))
+//     throw new CustomError.BadRequestError("Please upload an image.");
+
+//   // Optionally you can limit the size of images to 10mb
+//   const maxSize = 1024 * 1024 * 10;
+//   if (img.size > maxSize) {
+//     throw new CustomError.BadRequestError("Image must be less than 5mb.");
+//   }
+//   // Use the path module to assign the image to a path you want it to be saved. As seen below.
+//   const imgPath = path.resolve(__dirname, "../uploads", `${img.name}`);
+
+//   console.log(imgPath);
+//   // Use the express fileUpload method mv() to move the image file to the folder. This is asynchronous
+//   await img.mv(imgPath);
+
+  
+//   const file = fs.readFileSync(imgPath);
+
+//   console.log(file);
+//   const image = await imagekit.upload({
+//     file: file, //required
+//     fileName: "image", //requiredextensions:
+//     ["tag1", "tag2"], 
+//     extensions: [
+//       {
+//         name: "google-auto-tagging", //"aws-auto-tagging":
+//         maxTags: 20,
+//         minConfidence: 65,
+//       },
+//     ],
+//   });
+//   res.status(StatusCodes.OK).json({ image });
+// };
 
 module.exports = {
   uploadImage,
